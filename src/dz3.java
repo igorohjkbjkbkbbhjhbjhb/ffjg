7722a9d2137460b5ca0ac8b8b5e19ddb9d64df43
igork <igor.khys@mail.ru>
2025-06-21 16:12:27 +0300

    dsasdaas

---
+++ src/dz3.java
@@ -0,0 +1,24 @@
+public class dz3 {
+    public static void main(String[] args) {
+        for (int i = 1;i<11;++i){
+            System.out.println(i);
+        }
+        //2
+        System.out.println();
+        for (int i = 0;i<=21;i+=2){
+            System.out.println(i);
+        }
+        //3
+        for (int i =7;i<=98;i+=7){
+            System.out.print(i +" ");
+        }
+        //4
+        var u = -12000;
+        for (int i = 0;i<13;++i){
+            u += 12000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + u);
+        }
+
+    }
+    }
+
