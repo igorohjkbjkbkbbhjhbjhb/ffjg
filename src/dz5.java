44bf245d742670c644e760789932e676db965e76
igork <igor.khys@mail.ru>
2025-06-28 23:09:25 +0300

    dsasdaas

---
+++ src/dz5.java
@@ -0,0 +1,73 @@
+public class dz5 {
+    public static void main(String[] args) {
+        int[] a ={1,2,3,4,5,6,8,9,0,7};
+        double e =0;
+
+        for (int i = 0;i<a.length; i++){
+        e += a[i];
+        }
+        double r = e /a.length;
+        System.out.println( r);
+        //2
+        int[]ert ={0,-1,-2,-3,-4,-5,-6,7,-8,-9};
+        for (int i =0;i<ert.length;i++){
+            if(ert[i]<0){
+               ert[i]=ert[i]*-1;
+
+            }
+            System.out.print(ert[i] + " ");
+
+        }
+        //4
+        int[]y ={1,2,3,4,5,5,6,7,8,8,9,9,8,76,5,4,};
+        var u = 0;
+        for (int i = 0;i<y.length;i++){
+            u=i;
+        }
+        System.out.println(u);
+        //3
+        var op=0;
+        int[][]o ={{0,1,2,3},{4,5,6,7},{8,9,10,11}};
+        var l = 0;
+        for (int g =0; g<o.length;g++){
+            op *=0;
+            l = g;
+            for (int i=0;i<o[g].length;i++){
+                op += o[g][i];
+
+            }
+            System.out.println(op-l);
+
+        }
+        //5
+        int[]procioros ={0,1,2,3,4,7,4,5,4,3};
+        var gh =0;
+        for (int i =0; i<procioros.length;i++){
+            gh = procioros[i];
+            if (gh==procioros[i+=1]){
+                System.out.println();
+            }
+            for (gh)
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
