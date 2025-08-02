d6d4f1c3f5244c1dae95802d192cf2c9a3786d04
igork <igor.khys@mail.ru>
2025-08-02 11:58:34 +0300

    dz6

--- src/dz6.java
+++ src/dz6.java
@@ -1,14 +1,12 @@
 public class dz6 {
     public static void main(String[] args) {
-        int[] y ={1,2,3,4,5,6,7,8,9,0};
-        for (int a: y){
-            System.out.println(a);
-        }
-        for (int i = 0; i< y.length;i++){
-            System.out.println(y[i] * 2);
-        }
-
-
+                int[] y ={1,2,3,4,5,6,7,8,9,0};
+                for (int a: y){
+                    System.out.println(a);
+                }
+                for (int i = 0; i< y.length;i++){
+                    System.out.println(y[i] * 2);
+                }
 
 
     }
