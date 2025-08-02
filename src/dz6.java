ca6df52681fc18fa193604594b5005787822caf5
igork <igor.khys@mail.ru>
2025-08-02 11:54:48 +0300

    dz6

---
+++ src/dz6.java
@@ -0,0 +1,15 @@
+public class dz6 {
+    public static void main(String[] args) {
+        int[] y ={1,2,3,4,5,6,7,8,9,0};
+        for (int a: y){
+            System.out.println(a);
+        }
+        for (int i = 0; i< y.length;i++){
+            System.out.println(y[i] * 2);
+        }
+
+
+
+
+    }
+}
